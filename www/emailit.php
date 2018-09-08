f97959dcfd05c078bd6666e6c930d74d1e4ca663
root <root@ip-172-31-32-92.us-west-2.compute.internal>
2018-09-08 23:55:34 +0000

    modified:   emailit.php

--- www/emailit.php
+++ www/emailit.php
@@ -3,13 +3,13 @@
 $from = "test@piscitello.org";
 $to = "frank@piscitello.org";
 $subject = "Testing Email";
-$message = "CALL: " . $_POST["CallType"];
-$message = $message . "PLACE: Public Safety";
-$message = $message . "ADDR: 690 S Church St";
-$message = $message . "CITY: West Chester";
-$message = $message . "ID: 2018-FJP-011";
-$message = $message . "PRI: TEST Message";
-$message = $message . "INFO: Pt states the back of his eye hurts when he urinates. M/44. No resp. Conscious. Needs more psych meds. Prefers John's Hopkins ER. Not that CCH stuff.";
+$message = "CALL: " . $_POST["CallType"] . "\n";
+$message = $message . "PLACE: Public Safety" . "\n";
+$message = $message . "ADDR: 690 S Church St" . "\n";
+$message = $message . "CITY: West Chester" . "\n";
+$message = $message . "ID: 2018-FJP-011" . "\n";
+$message = $message . "PRI: TEST Message" . "\n";
+$message = $message . "INFO: Pt states the back of his eye hurts when he urinates. M/44. No resp. Conscious. Needs more psych meds. Prefers John's Hopkins ER. Not that CCH stuff." . "\n";
 
 $headers = "From:" . $from;
 mail($to,$subject,$message,$headers);
