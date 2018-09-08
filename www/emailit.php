3ea2328cd33ef31216de090a342083b06a6df87a
root <root@ip-172-31-32-92.us-west-2.compute.internal>
2018-09-08 23:49:40 +0000

    modified:   emailit.php

--- www/emailit.php
+++ www/emailit.php
@@ -4,12 +4,12 @@ $from = "test@piscitello.org";
 $to = "frank@piscitello.org";
 $subject = "Testing Email";
 $message = "CALL: " . $_POST["CallType"];
-$message = "PLACE: Public Safety";
-$message = "ADDR: 690 S Church St";
-$message = "CITY: West Chester";
-$message = "ID: 2018-FJP-011";
-$message = "PRI: TEST Message";
-$message = "INFO: Pt states the back of his eye hurts when he urinates. M/44. No resp. Conscious. Needs more psych meds. Prefers John's Hopkins ER. Not that CCH stuff.";
+$message = $message . "PLACE: Public Safety";
+$message = $message . "ADDR: 690 S Church St";
+$message = $message . "CITY: West Chester";
+$message = $message . "ID: 2018-FJP-011";
+$message = $message . "PRI: TEST Message";
+$message = $message . "INFO: Pt states the back of his eye hurts when he urinates. M/44. No resp. Conscious. Needs more psych meds. Prefers John's Hopkins ER. Not that CCH stuff.";
 
 $headers = "From:" . $from;
 mail($to,$subject,$message,$headers);
