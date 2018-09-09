51ad4d2452e8ddced298eaf9f9817997eca1343e
root <root@ip-172-31-32-92.us-west-2.compute.internal>
2018-09-09 02:06:26 +0000

    new file:   calltypes.php

---
+++ www/calltypes.php
@@ -0,0 +1,5 @@
+<option name="CallType" value="ETOH-Eval">Alcohol Evaluation</option>
+<option name="CallType" value="Unresponsive">Unresponsive Person</option>
+<option name="CallType" value="Syncope">Feinting</option>
+<option name="CallType" value="Injured">Injured Person</option>
+
