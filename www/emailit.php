721b9a84c1b15d923b3f03931c3f58d5cefcb9df
fpiscite <fpiscitell@wcupa.edu>
2018-09-09 17:31:47 +0000

    modified:   emailit.php

--- www/emailit.php
+++ www/emailit.php
@@ -1,29 +1,51 @@
 <?php
-$locations = array(
+ 
+$locations = array( 
 	"ALG" => array("ALG","Allegheny Hall","120 University Ave"),
 	"PEO" => array("PEO","Peoples Building","690 S Church St"),
-	"SSS" => array("SSS","Schmucker South","750 S Church St")
+	"SSS" => array("SSS","Schmucker South","750 S Church St"),
+	"ZZZ" => array("ZZZ","Other Campus Location","")
 	);
-$buildingCode = $_POST["location"];
+constant(BUILDINGCODE,0);
+constant(BUILDINGNAME,1);
+constant(BUILDINGADDR,2);
+
+$buildingCode = clean_data($_POST["location"]);
+$city = clean_data("West Chester");
+$callType = clean_data($_POST["CallType"]);
+$ID = clean_data("2018-WCUEMS-1");
+$PRI = clean_data("Test Message");
+$Room = clean_data($_POST["room"]);
+$Age = clean_data($_POST["age"]);
+$AVPU = clean_data($_POST["alertness"]);
+$Notes = clean_data($_POST["notes"]);
+
 
 $from = "test@piscitello.org";
 $to = "frank@piscitello.org";
 $subject = "Testing Email";
-$message = "CALL: " . $_POST["CallType"] . "\n";
-$message = $message . "PLACE: " . $locations[$buildingCode][0] . "\n";
-$message = $message . "ADDR: ". $locations[$buildingCode][1] . "\n";
-$message = $message . "CITY: West Chester" . "\n";
-$message = $message . "ID: 2018-FJP-011" . "\n";
-$message = $message . "PRI: TEST Message" . "\n";
-$message = $message . "ROOM: " . $_POST["room"] . "\n";
+$message = "CALL: " . $callType . "\n";
+$message = $message . "PLACE: " . $locations[$buildingCode][BUILDINGNAME] . "\n";
+$message = $message . "ADDR: ". $locations[$buildingCode][BUILDINGADDR] . "\n";
+$message = $message . "CITY: ". $city . "\n";
+$message = $message . "ID: ". $ID . "\n";
+$message = $message . "PRI: ". $PRI . "\n";
+$message = $message . "ROOM: " . $room . "\n";
 $message = $message . "INFO: ";
-$message = $message . "Age: " . $_POST["age"] . "\n";
-$message = $message . "AVPU: " . $_POST["alertness"] . "\n";
-$message = $message . $_POST["notes"] . "\n";
+$message = $message . "Age: " . $Age . "\n";
+$message = $message . "AVPU: " . $AVPU . "\n";
+$message = $message . $Notes . "\n";
 
 $headers = "From:" . $from;
 #mail($to,$subject,$message,$headers);
 echo $message;
 echo "Message sent.";
+
+function clean_data($data) {
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
 
 ?>
