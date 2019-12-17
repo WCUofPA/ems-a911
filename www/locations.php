c7c47bcedc89878ce1aeb74e7b61b30d870bbc80
Frank Piscitello <fpiscitell@wcupa.edu>
2019-12-17 12:27:25 -0500

    Update locations.php

--- www/locations.php
+++ www/locations.php
@@ -1,3 +1,13 @@
 <option value="PEO">Peoples Building</option>
 <option value="SSS">Schmucker Science South</option>
 <option value="SSN">Schmucker Science North</option>
+<?php
+foreach ($locations as $building)
+{
+  print "<option value=\""
+    print $building[0];
+    print "\">";
+    print $Building[1];
+    print "</option>";
+}
+?>
