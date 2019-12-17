0e6a5b24d42d2de6abd445044963bff27a9bd6f4
Frank Piscitello <fpiscitell@wcupa.edu>
2019-12-17 14:16:36 -0500

    Update locations.php

--- www/locations.php
+++ www/locations.php
@@ -4,7 +4,7 @@ foreach ($locations as $buildingCode => $BuildingName)
     echo "<option value=\"";
     echo $buildingCode;
     echo "\">";
-    echo key($buildingName);
+    echo key($buildingCode);
     echo "</option>";
 }
 ?>
