af473cf0c6800f9e261fc3200f04bdc26e4770c3
root <root@ip-172-31-46-163.us-west-2.compute.internal>
2019-12-19 16:22:43 -0500

    Hi

--- www/calltypes.php
+++ www/calltypes.php
@@ -1,4 +1,5 @@
 <option value="ETOH-Eval">Alcohol Evaluation</option>
+<option value="TEST PAGE - IGNORE">Test Paging</option>
 <option value="Unresponsive">Unresponsive Person</option>
 <option value="Syncope">Feinting</option>
 <option value="Injured">Injured Person</option>
--- www/emailit.php
+++ www/emailit.php
@@ -1,10 +1,11 @@
 <?php include "variables.php" ?>
 <?php
+require_once "Mail.php";
 
 $buildingCode = clean_data($_POST["location"]);
 $city = clean_data("West Chester");
 $callType = clean_data($_POST["CallType"]);
-$ID = clean_data("2018-WCUEMS-1");
+$ID = clean_data($_POST["ID"]);
 $PRI = clean_data("Test Message");
 $Room = clean_data($_POST["room"]);
 $Age = clean_data($_POST["age"]);
@@ -13,8 +14,8 @@ $Notes = clean_data($_POST["notes"]);
 
 
 $from = "test@piscitello.org";
-#$to = "frank@piscitello.org";
-$to = "27576-Yz77em2hsQfTiCPv@alert.active911.com";
+$to = "frank@piscitello.org";
+#$to = "PZ27576-Yz77em2hsQfTiCPv@alert.active911.com";
 $subject = "WCUPA EMS DISPATCH";
 $message = "CALL: " . $callType . "\n";
 $message = $message . "PLACE: " . $locations[$buildingCode][BUILDINGNAME] . "\n";
@@ -23,15 +24,27 @@ $message = $message . "CITY: ". $city . "\n";
 $message = $message . "ID: ". $ID . "\n";
 $message = $message . "PRI: ". $PRI . "\n";
 $message = $message . "ROOM: " . $Room . "\n";
-$message = $message . "INFO: ";
-$message = $message . "Age: " . $Age . "\n";
+$message = $message . "AGE: " . $Age . "\n";
 $message = $message . "AVPU: " . $AVPU . "\n";
-$message = $message . $Notes . "\n";
+$message = $message . "INFO: " . $Notes . "\n";
 
-$headers = "From:" . $from;
-mail($to,$subject,$message,$headers);
+#$headers = "From:" . $from;
+#mail($to,$subject,$message,$headers);
 echo $message;
-echo "Message sent.";
+#echo "Message sent.";
+
+$headers = array ('From' => $from,
+	'To' => $to,
+	'Subject' => $subject);
+$smtp = Mail::factory('smtp');
+$mail = $smtp->send($to, $headers, $message);
+if (PEAR::isError($mail) {
+	echo ("<p>" . $mail->getMessage() . "</p>");
+}
+else
+{
+echo ("<p>Message successfully sent!</p>");
+}
 
 function clean_data($data) {
 	$data = trim($data);
